8a10d256632bf53ae1181b071e4cf4f8f2dcd90c
malomarrec <malo.marrec@gmail.com>
2021-02-22 10:06:51 +0100

    add secret debug flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,10 @@ chmod +x src
 # TODO: iterare over campaigns in checkout
 # Launch campaigns
 export SRC_ENDPOINT=https://demo.sourcegraph.com
-export SRC_ACCESS_TOKEN=${{ secrets.SRC_ACCESS_TOKEN }}
+export SRC_ACCESS_TOKEN=${{secrets.SRC_ACCESS_TOKEN}}
+echo ${{secrets.TEST_SECRET}}
 
-cd /github/workspace
+cd github/workspace
 
 src campaigns apply -f my.campaign.yml
 
